9942fbf097cb1df3d4c0bdfce89172184bfa4801
atippaz <nasakun13201@gmail.com>
2023-10-03 18:05:34 +0700

    save change

--- assets/js/startup.js
+++ assets/js/startup.js
@@ -1,18 +1,36 @@
 import _api from './api.js'
 const api = _api()
-const githubData = []
+const allGithubData = []
 async function startUp(){
   const repos = document.getElementById('repos')
-  const data =  await api.fetchAllRepositories()
+  const repo_count = document.getElementById('repo_count')
+  let data =  await api.fetchAllRepositories()
+  repo_count.innerText = `(${data.length})`
+  allGithubData.push(...data)
+
   console.log(data);
+  data = data.filter(x=> x.has_pages || (x.homepage !== "" && x.homepage !== null) && !x.fork)
   data.forEach(async e=>{
     const container = document.createElement('div')
-    container.innerText = e.name
+    const a = document.createElement('a')
+    const a2 = document.createElement('a')
+
+    a.href = e.has_pages ? "":e.homepage
+    a2.href = e.html_url
+    a.innerText = 'see website'
+a2.innerText = 'see repo'
     // githubData.push({name:e.name,has_deploy:(await api.fetchApi(e.deployments_url)).length})
     // const deployment = await api.fetchApi(e.deployments_url)
     // console.log(deployment); 
+    container.innerText = e.name
+    container.appendChild(document.createElement('br'))
+    container.appendChild(a)
+    container.appendChild(document.createElement('br'))
+
+    container.appendChild(a2)
     repos.appendChild(container)
   })
+  console.log(data);
   // console.log(githubData);
   // const deployment = await api.fetchApi(data[32].deployments_url)
     // console.log(deployment);
